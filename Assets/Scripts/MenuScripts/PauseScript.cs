bd318e721bda0fc08363bb3aa6215b257bddfc49
jwruijzenaars <jw.ruijzenaars@student.avans.nl>
2024-05-12 16:15:40 +0200

    Created simple pause screen with pause/unpause bound to E key
    - Screen pops up when you press E
    - Screen pauses the game
    - Screen goes away by pressing the button or E key

---
+++ Assets/Scripts/MenuScripts/PauseScript.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class PauseScript : MonoBehaviour
+{
+    public GameObject pauseScreen;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E) && pauseScreen.activeSelf)
+        {
+            Time.timeScale = 1;
+            pauseScreen.SetActive(false);
+        }
+        else if (Input.GetKeyDown(KeyCode.E) && !pauseScreen.activeSelf)
+        {
+            Time.timeScale = 0;
+            pauseScreen.SetActive(true);
+        }
+    }
+
+    public void Pause()
+    {
+        if (pauseScreen.activeSelf)
+        {
+            Time.timeScale = 1;
+            pauseScreen.SetActive(false);
+        }
+        else if (!pauseScreen.activeSelf)
+        {
+            Time.timeScale = 0;
+            pauseScreen.SetActive(true);
+        }
+    }
+}
