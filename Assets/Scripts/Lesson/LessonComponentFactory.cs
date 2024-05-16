c56e132a609686dc5e286d0742ed42fb7d76c573
JaschavdA <94369567+JaschavdA@users.noreply.github.com>
2024-05-16 13:08:16 +0200

    added lesson component factory

---
+++ Assets/Scripts/Lesson/LessonComponentFactory.cs
@@ -0,0 +1,35 @@
+using Assets.Scripts.Other;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LessonComponentFactory : ScriptableObject
+{
+
+    //TODO: Keep expanding this as more lesson components are made.
+    public List<LessonComponent> createLessonComponentsFromPlayerPrefs()
+    {
+        string[] selectedLessonComponentStrings = PlayerPrefs.GetString("SelectedLessonComponents").Split(',');
+        List<LessonComponent> components = new List<LessonComponent>();  
+        foreach (string str in selectedLessonComponentStrings)
+        {
+            switch (str)
+            {
+                case "Kahoot": components.Add(CreateKahootComponent());
+                    break;
+            }
+        }
+        return components;
+    }
+
+    public LessonComponent CreateKahootComponent()
+    {
+        var component = ScriptableObject.CreateInstance<LessonComponent>();
+        component.name = "Kahoot";
+        component.modifiers.Add(CompetenceType.CONNECTION, 0.4f);
+        component.modifiers.Add(CompetenceType.COMPETENCE, 0.8f);
+        component.modifiers.Add(CompetenceType.AUTONOMY, 0.1f);
+        return component;
+
+    }
+}
